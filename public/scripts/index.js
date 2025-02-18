5b9cebea825a19257b961b1e2795fb8b498830e1
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-02-18 10:45:53 +0100

    Forwards compatibility for Hotwired turbo (C5.5)

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,18 @@
 import Installer from "./Installer/Installer"
 
-document.addEventListener('DOMContentLoaded', () => {
-    const installer = new Installer('de') // ToDo: set locale
-
+const bindProductInstaller = (pi) => {
     document.getElementById('product-installer')?.addEventListener('click', (e) => {
         e.preventDefault()
-        installer.open()
+        pi.open()
     })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const pi = new Installer('de')
+    bindProductInstaller(pi)
 })
+
+document.addEventListener("turbo:load", () => {
+    const pi = new Installer('de')
+    bindProductInstaller(pi)
+});
